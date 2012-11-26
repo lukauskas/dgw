a57e8250df559501ae62a2d7239b684426fef17f
Saulius Lukauskas <luksaulius@gmail.com>
2012-11-26 22:40:15 +0000

    added support for resolution in peaks

--- peak.py
+++ peak.py
@@ -2,19 +2,22 @@ from collections import namedtuple
 from itertools import izip
 Point = namedtuple('Point', ["location", "type"])
 
-class Peak:
+    
+class Peak(object):
     
     chromosome = None
     __start      = None
     __end        = None
+    __resolution = None
     
     __data     = None
     __points_of_interest = None
     
-    def __init__(self, chromosome, start, end):
+    def __init__(self, chromosome, start, end, resolution=1):
         self.chromosome = chromosome
         self.__start      = int(start)
         self.__end        = int(end)
+        self.__resolution = resolution
         
         self.data = []
         self.points_of_interest = set()
@@ -58,14 +61,35 @@ class Peak:
     @data.setter
     def data(self, value):
         self.__data = value
-
-    def data_relative_to_point(self, point):
-        for pos, n in izip(xrange(self.start, self.end), self.data):
+    
+    @property
+    def resolution(self):
+        return self.__resolution
+    
+    def __data_bin_of_point(self, point):
+        adjusted_point = point - self.start
+        bin_id = adjusted_point / self.resolution
+        return bin_id
+        
+    def expanded_data_relative_to_point(self, point):
+        for pos in xrange(self.start, self.end):
+            bin_of_pos = self.__data_bin_of_point(pos)
+            n = self.data[bin_of_pos]
             adjusted_pos = pos - point
             yield (adjusted_pos, n)
     
+    def data_relative_to_point(self, point):
+        point_bin = self.__data_bin_of_point(point)
+        
+        for i, n in enumerate(self.data):
+            rel_bin = i - point_bin
+            yield(rel_bin, n)
+            
     def data_relative_to_start(self):
         return enumerate(self.data)
+    
+    def expanded_data_relative_to_start(self):
+        return self.expanded_data_relative_to_point(self.start)
     
     def find_interesting_points_from_set(self, interesting_points_set):
         for pos in xrange(self.start, self.end):
