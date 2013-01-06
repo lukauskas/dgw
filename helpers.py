b7e9dbe97337dd1c6a7628a2b281573876acba84
Saulius Lukauskas <luksaulius@gmail.com>
2013-01-06 16:40:24 +0200

    various fixes for helpers

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
-from math import floor, ceil
+from math import floor, ceil, factorial
 import pandas as pd
 import pysam
 import numpy as np
+import scipy.spatial.distance
+from itertools import combinations, izip
 
 def read_bed(bed_file, resolution=1):
     peaks = pd.read_csv(bed_file, sep="\t", header=None)
@@ -9,7 +11,7 @@ def read_bed(bed_file, resolution=1):
     peaks.columns = ['chromosome', 'start', 'end', 'name', 'score']
     peaks = peaks.set_index('name')
 
-    peaks = clip_to_fit_resolution(peaks, resolution)
+    peaks = clip_to_fit_resolution2(peaks, resolution)
     return peaks
 
 def join_with_length_information(peaks_df):
@@ -18,6 +20,47 @@ def join_with_length_information(peaks_df):
     lens.name = 'length'
 
     return peaks_df.join(lens)
+
+def choose(n, k):
+    return factorial(n) / ((factorial(n-k)) * factorial(k))
+
+def find_prototype(dm, full_index, cluster_indices):
+
+    # If we have only one element in the cluster, it is its own prototype
+    if len(cluster_indices) == 1:
+        return list(cluster_indices)[0], 0
+
+    sums = {}
+    for (i, j), dist in izip(combinations(full_index, 2), dm):
+        if i not in cluster_indices:
+            continue
+
+        if j not in cluster_indices:
+            continue
+
+        try:
+            sums[i] += dist
+        except KeyError:
+            sums[i] = dist
+
+        try:
+            sums[j] += dist
+        except KeyError:
+            sums[j] = dist
+
+    min_key = None
+    min_value = None
+    for key, value in sums.iteritems():
+        if min_value is None or min_value > value:
+            min_value = value
+            min_key = key
+
+    return min_key, min_value
+
+def distance_between(dm, n, i, j):
+    if i == j:
+        return 0
+    return dm[len(dm) - choose(n-i,2) + j - i - 1]
 
 def distances_to_previous_peaks(peaks_df):
 
@@ -92,36 +135,96 @@ def clip_to_fit_resolution(peaks, resolution=1):
 
     return peaks
 
+def clip_to_fit_resolution2(peaks, resolution=1):
+    if resolution == 1:
+        return peaks
+
+    lens = peaks['end'] - peaks['start']
+    lens.name = 'length'
+    peaks = peaks.join(lens)
+
+    new_peaks_data = []
+    new_peaks_index = []
+
+    for ix, row in peaks.iterrows():
+        remainder = row['length'] % resolution
+
+        if remainder == 0:
+            offset_needed = 0
+        else:
+            offset_needed = resolution - remainder
+
+        add_left = offset_needed / 2
+        add_right = offset_needed / 2 + offset_needed % 2
+
+
+        row['start'] -= add_left
+        row['end']   += add_right
+
+        new_peaks_data.append(row[['chromosome', 'start', 'end']])
+        new_peaks_index.append(ix)
+
+        assert((row['end'] - row['start']) % resolution == 0)
+
+
+    new_peaks = pd.DataFrame(new_peaks_data, index=new_peaks_index)
+    return new_peaks
+
+
+
+
+
 def get_read_count_for_region(samfile, chromosome, start, end, resolution=1):
+    '''
+        Returns read count data for a samfile.
+        Chromosome, start, and end all describe a region of the data.
+        This should be zero indexed and the last coordinate is not included, following BED format.
 
-    data_arr = []
+        The read count is returned aggregated by number of base pairs equal to resolution parameter.
+        That is if resolution is 10 the data will be returned for every 10 base pairs.
 
-    # Note this has to be peak.end - 1
-    # because samfile.pileup treats ends inclusively
+        Note that end-start mod resolution should be zero as the function will not know what to do otherwise.
+
+    :param samfile:
+    :param chromosome:
+    :param start:
+    :param end:
+    :param resolution:
+    :return:
+    '''
+
+
+    if (end - start) % resolution != 0:
+        raise ValueError('The resolution {0} is not valid for peak of length {1}'.format(resolution, end-start))
+
+    # Initialise
     data_len = (end - start) / resolution
-    #    return numpy.random.rand(data_len)
+    data_buffer = np.zeros(data_len)
 
-    for i in xrange(data_len):
-        read_from = start + i * resolution
-        read_to   = read_from + resolution - 1 # samfile treats ends inclusively
-        assert(start <= read_from <= read_to <= end)
+    # Fetch all alignments from samfile
+    alignments = samfile.fetch(chromosome, start, end)
 
-        count = samfile.count(chromosome, read_from, read_to)
-        data_arr.append(count)
+    for alignment in alignments:
+        assert(alignment.aend > alignment.pos)
+        start_bin = max(0, (alignment.pos-start) / resolution)
+        end_bin   = min(data_len -1, (alignment.aend-start - 1) / resolution)
 
-    assert(len(data_arr) > 0)
-    assert(len(data_arr) == data_len)
-    return data_arr
+        data_buffer[start_bin:end_bin+1] += 1
+
+    return data_buffer
 
 
 def read_peak_data_from_bam(alignments_file, peaks, resolution=1):
     '''
+    Returns data from bam for the specified peaks.
+    Peaks should be a pandas.DataFrame object that has 'chromosome', 'start' and 'end' columns.
+        Both start and end should be zero-indexed. End coordinate is not included
+        This follows BED format of data.
 
-    @param alignments_file:
-    @param peaks:
-    @type peaks: pd.DataFrame
-    @param resolution:
-    @return:
+    :param alignments_file:
+    :param peaks:
+    :param resolution:
+    :return:
     '''
 
     samfile = pysam.Samfile(alignments_file, 'rb')
@@ -149,8 +252,11 @@ def read_peak_data_from_bam(alignments_file, peaks, resolution=1):
     N = len(peak_data)
 
     # Add NaNs to offsets we do not know peak locations of
-    peak_data = [ x + [np.nan] * (max_length - len(x)) for x in peak_data]
+    peak_data_np_arr = np.empty([N, max_length])
+    for i, x in enumerate(peak_data):
+        peak_data_np_arr[i] = np.hstack((x,[np.nan] * (max_length - len(x))))
 
+    peak_data = peak_data_np_arr
 #    arr = np.array(peak_data)
     # Create a sparse DataFrame with peak alignments
     sdf = pd.DataFrame(peak_data)
