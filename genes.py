b0f31048ae15bf0b7024fc0290ee5552de396fcf
Saulius Lukauskas <luksaulius@gmail.com>
2013-01-06 17:32:08 +0200

    make sure to use TES not TSS for negative peaks

--- genes.py
+++ genes.py
@@ -4,6 +4,15 @@ import pandas as pd
 import helpers
 
 def read_known_genes_file(known_genes_filename):
+    '''
+    Reads known_genes file.
+    The file can be obtained from the
+    ``table browser from ENCODE project <http://encodeproject.org/cgi-bin/hgTables?hgsid=320204017&clade=mammal&org=Human&db=hg19&hgta_group=genes&hgta_track=wgEncodeRegTxn&hgta_table=0&hgta_regionType=genome&position=chr21%3A33%2C031%2C597-33%2C041%2C570&hgta_outputType=wigData&hgta_outFileName=>``
+
+    :param known_genes_filename:
+    :return:
+    :rtype : pd.DataFrame
+    '''
 
     known_genes = pd.read_csv(known_genes_filename, sep='\t')
 
@@ -27,16 +36,28 @@ def read_known_genes_file(known_genes_filename):
 
     return known_genes
 
-def get_tss_peak_df(known_genes, window, resolution=1):
-    known_genes = known_genes[['chromosome', 'start']].drop_duplicates()
+def get_regions_around_tss(known_genes, window, resolution=1):
+    '''
+    Returns regions that are around transcription starting sites.
+    For genes on a negative strand, transcription end sites are used.
 
-    starts = known_genes['start'] - window
-    ends   = known_genes['start'] + window
+    :param known_genes: parsed known_genes file, see read_known_genes_file
+    :param window: window of the number of base pairs to add to region. A window of 2000 would return from -2000 to +2000
+                   base pairs around the TSS
+    :param resolution: Resolution of data to clip the window around.
+    :return:
+    '''
+    # Get the locations of tss. Note that they coinside with Transcription End Site for peaks on negative strand
+    tss_locations = known_genes[known_genes.strand == '+']['start'].append(known_genes[known_genes.strand == '-']['end'])
 
+    # Add window around these
+    starts = tss_locations - window
+    ends   = tss_locations + window
 
     peak_df = pd.DataFrame({'chromosome' : known_genes['chromosome'],
                             'start' : starts,
                             'end' : ends}, index=known_genes.index)
+
     peak_df = helpers.clip_to_fit_resolution2(peak_df, resolution=resolution)
     peak_df = peak_df.drop_duplicates()
     return peak_df
