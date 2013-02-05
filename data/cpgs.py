acacc5117b52d3be05409c648d6de11a32fa5da0
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-05 10:32:06 +0000

    cpg data parser

---
+++ data/cpgs.py
@@ -0,0 +1,23 @@
+__author__ = 'saulius'
+import pandas as pd
+
+def read_cpgs(cpgs_filename):
+    cpgs = pd.read_csv(cpgs_filename, sep='\t')
+
+    cols = list(cpgs.columns)
+    cols = map(lambda x : x.strip('#'), cols)
+
+    # Rename cols
+    new_cols = []
+    for c in cols:
+        if c == 'chrom':
+            c = 'chromosome'
+        elif c == 'chromStart':
+            c = 'start'
+        elif c == 'chromEnd':
+            c = 'end'
+        new_cols.append(c)
+
+    cpgs.columns = new_cols
+
+    return cpgs
