c83d92f280a57e3369dc5eaef950cd158b5522b3
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-09 23:16:04 +0000

    refactored functions in data.genes to work with newly-created Genes object

--- data/genes.py
+++ data/genes.py
@@ -1,17 +1,57 @@
 __author__ = 'saulius'
 
 import pandas as pd
+from containers import Regions
+
+class Genes(Regions):
+
+    def transcription_start_sites(self):
+        """
+        Returns transcription start site locations for the current set of Genes.
+        Pays attention to the strand of the gene and returns the start of the gene for positive strand genes
+        and the end of the gene for the negative strand genes.
+
+        :rtype: `pd.Series`
+        """
+        start_sites = self[self.strand=='+']['start'].append(self[self.strand=='-']['end'])
+        start_sites = start_sites.ix[self.index] # reindex the data using original order
+
+        return start_sites
+
+    def regions_around_transcription_start_sites(self, window_width):
+        """
+        Returns a `Regions` object corresponding to the locations from -window_width to +window_width around the
+        transcription start sites for these genes
+
+        :param window_width: the width of the window
+        :type window_width: int
+        :return: regions around tss
+        :rtype: `Regions`
+        """
+
+        tss_locations = self.transcription_start_sites()
+
+        # Add window around these
+        starts = tss_locations - window_width
+        ends   = tss_locations + window_width
+
+        regions_df = pd.DataFrame({'chromosome' : self['chromosome'],
+                                'start' : starts,
+                                'end' : ends}, index=self.index)
+
+        return Regions(regions_df)
+
 
 def read_known_genes(known_genes_filename):
-    '''
+    """
     Reads known_genes file.
     The file can be obtained from the
     ``table browser from ENCODE project <http://encodeproject.org/cgi-bin/hgTables?hgsid=320204017&clade=mammal&org=Human&db=hg19&hgta_group=genes&hgta_track=wgEncodeRegTxn&hgta_table=0&hgta_regionType=genome&position=chr21%3A33%2C031%2C597-33%2C041%2C570&hgta_outputType=wigData&hgta_outFileName=>``
 
     :param known_genes_filename:
     :return:
-    :rtype : pd.DataFrame
-    '''
+    :rtype: pd.DataFrame
+    """
 
     known_genes = pd.read_csv(known_genes_filename, sep='\t')
 
@@ -32,68 +72,7 @@ def read_known_genes(known_genes_filename):
     known_genes.columns = new_cols
     known_genes = known_genes.set_index('name')
 
-
-    return known_genes
-
-def tss_locations(known_genes):
-    '''
-    Returns locations of TSS. Either uses gene['start'] or gene['end'] depending on whether the gene is on '+' or '-' strand.
-
-    :param known_genes:
-    :return:
-    '''
-
-    locations = known_genes[known_genes.strand=='+']['start'].append(known_genes[known_genes.strand=='-']['end'])
-    locations = locations.ix[known_genes.index] # reindex the data using original order
-
-    return locations
-
-def get_regions_around_tss(known_genes, window, resolution=1):
-    '''
-    Returns regions that are around transcription starting sites.
-
-    :param known_genes: parsed known_genes file, see read_known_genes_file
-    :param window: window of the number of base pairs to add to region. A window of 2000 would return from -2000 to +2000
-                   base pairs around the TSS
-    :param resolution: Resolution of data to clip the window around.
-    :return:
-    '''
-    # Get the locations of tss. Note that they coinside with Transcription End Site for peaks on negative strand
-    tss_locations = tss_locations(known_genes)
-
-    # Add window around these
-    starts = tss_locations - window
-    ends   = tss_locations + window
-
-    peak_df = pd.DataFrame({'chromosome' : known_genes['chromosome'],
-                            'start' : starts,
-                            'end' : ends}, index=known_genes.index)
-
-    peak_df = helpers.clip_to_fit_resolution2(peak_df, resolution=resolution)
-    peak_df = peak_df.drop_duplicates()
-    return peak_df
-
-def genes_that_may_overlap_region(known_genes, region, overlap_window=2000):
-    chr_genes = known_genes[known_genes.chromosome == region['chromosome']]
-
-    transcription_starts = tss_locations(chr_genes)
-    overlaps = transcription_starts[transcription_starts.between(region['start'] - overlap_window,
-                                                                     region['end'] + overlap_window)]
-
-    overlap_indices = overlaps.index - [region.name] # Remove the region provided from list
-
-    return known_genes.ix[overlap_indices]
-
-def overlapping_gene_counts(known_genes, regions, overlap_window=2000):
-
-    ixs = []
-    counts = []
-
-    for ix, region in regions.iterrows():
-        ixs.append(ix)
-        counts.append(len(genes_that_may_overlap_region(known_genes, region, overlap_window=overlap_window)))
-
-    return pd.Series(counts, index=ixs)
+    return Genes(known_genes)
 
 def read_gtf(gtf_location):
     '''
@@ -132,4 +111,4 @@ def read_gtf(gtf_location):
     first_columns = ['seqname', 'start', 'end', 'strand', 'gene_name', 'gene_status', 'gene_type', 'score', 'level']
     data = pd.DataFrame(data, columns=first_columns + list(data.columns - first_columns))
 
-    return data
+    return Genes(data)
