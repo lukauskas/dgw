078a6e40bb10e65242142a92cd62a76d7cf68a55
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-17 13:02:49 +0000

    added chunkedio so it does not get lost, but it does not seem it works at the moment

--- data/storage.py
+++ data/storage.py
@@ -1,1 +1,63 @@
-__author__ = 'saulius'
+import io
+
+
+class ChunkedIO(io.IOBase):
+    """
+    Wrapper around file object that would chunk read and write operations
+    as to fix any nonsenses that tend to happen due to incorrect handling
+    of large objects.
+
+    see Issues #2806, #574 for instance
+    """
+    __max_chunk_size = None
+    __file = None
+
+    def __init__(self, file, max_chunk_size=2 ** 28):
+        """
+        Initialise `ChunkedIO` object with a file_like buffer.
+
+        :param file:
+        :param max_chunk_size: the maximum size of the chunks that will be read from or written to the file provided
+        :return:
+        """
+        max_chunk_size = int(max_chunk_size)
+        if max_chunk_size <= 0:
+            raise ValueError("Max chunk size should be greater than 0")
+
+        self.__max_chunk_size = max_chunk_size
+        self.__file = file
+
+    def read(self, size=None):
+        max_chunk_size = self.__max_chunk_size
+        if size is not None and size < max_chunk_size:
+            return self.__file.read(size)
+
+        data_read = ""
+        while size is None or size > 0:
+            read_size = max_chunk_size if size is None or size >= max_chunk_size else size
+            current_read = self.__file.read(size)
+            data_read += current_read
+            # If EOF was reached
+            if len(current_read) < read_size:
+                break
+            elif size is not None:
+                size -= len(current_read)
+
+        return data_read
+
+    def write(self, p_str):
+        max_chunk_size = self.__max_chunk_size
+
+        while len(p_str) > 0:
+            current_chunk_size = min(max_chunk_size, len(p_str))
+            current_chunk = p_str[:current_chunk_size]
+            p_str = p_str[current_chunk_size:]
+
+            self.__file.write(current_chunk)
+
+    def seek(self, *args, **kwargs):
+        self.file.seek(*args, **kwargs)
+
+    def close(self, *args, **kwargs):
+        self.__file.close(*args, **kwargs)
+
--- data/test_storage.py
+++ data/test_storage.py
@@ -1,1 +1,104 @@
-__author__ = 'saulius'
+import unittest
+import StringIO
+from storage import ChunkedIO
+import cPickle as pickle
+import tempfile
+import numpy as np
+
+class TestChunkedIOWithFakeFiles(unittest.TestCase):
+
+    def test_chunked_reading(self):
+        text = "some sample text"
+
+        f = StringIO.StringIO(text)
+        cf = ChunkedIO(f, max_chunk_size=3)  # Specify small chunk size here so it actually uses chunking
+        self.assertEqual(text, cf.read())
+
+    def test_chunked_reading_when_size_is_specified(self):
+
+        text = "some sample text"
+        f = StringIO.StringIO(text)
+
+        cf = ChunkedIO(f, max_chunk_size=5)
+
+        # Read size smaller than chunk size
+        self.assertEqual("so", cf.read(2))
+        # Read size equal to chunk size
+        self.assertEqual("me sa", cf.read(5))
+        # Read size greater than chunk size
+        self.assertEqual("mple te", cf.read(7))
+        # Read size greater than remaining string
+        self.assertEqual("xt", cf.read(10))
+
+    def test_chunked_writing(self):
+
+        text = "some sample text"
+        f = StringIO.StringIO()
+
+        cf = ChunkedIO(f, max_chunk_size=5)
+        cf.write(text)
+        self.assertEqual(text, f.getvalue())
+
+        f.close()
+    def test_chunked_writing_smaller_than_chunk_size(self):
+
+        text = "some sample text"
+        f = StringIO.StringIO()
+
+        cf = ChunkedIO(f, max_chunk_size=1000)
+        cf.write(text)
+
+        self.assertEqual(text, f.getvalue())
+        f.close()
+
+    def test_closing(self):
+
+        f = StringIO.StringIO()
+        cf = ChunkedIO(f)
+        cf.close()
+
+        self.assertTrue(f.closed)
+
+class TestChunkedIOPlaysNiceWithPickle(unittest.TestCase):
+
+    def test_pickle_dump_load_runtrip_works(self):
+
+        o = [1, 2, 3, 4, 5, 6]
+
+        f = StringIO.StringIO()
+        cf = ChunkedIO(f, max_chunk_size=2)
+
+        pickle.dump(o, cf, protocol=pickle.HIGHEST_PROTOCOL)
+        f.seek(0)
+
+        o2 = pickle.load(cf)
+
+        cf.close()
+
+        self.assertEqual(o, o2)
+
+
+class TestLargeFilePicklingToDisk(unittest.TestCase):
+
+    def test_large_np_array_pickling(self):
+        large_array = np.random.randn(300000000)
+        f = tempfile.TemporaryFile()
+        cf = ChunkedIO(f)
+        pickle.dump(large_array, cf, protocol=pickle.HIGHEST_PROTOCOL)
+        f.seek(0)
+        large_array2 = pickle.load(cf)
+        cf.close()
+
+        self.assertEqual(large_array, large_array2)
+
+    def test_large_np_array_np_save(self):
+        large_array = np.random.randn(300000000)
+        f = tempfile.TemporaryFile()
+        cf = ChunkedIO(f)
+        np.save(cf, large_array)
+        f.seek(-1)
+        large_array2 = pickle.load(cf)
+        cf.close()
+
+        self.assertEqual(large_array, large_array2)
+
