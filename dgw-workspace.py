fdeec7ee07f5842b7541c6b0c99d06f7c62f2bd7
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-19 22:12:33 +0000

    pd.load -> np.load

--- dgw-workspace.py
+++ dgw-workspace.py
@@ -3,7 +3,7 @@ import numpy as np
 import dgw.cluster.analysis
 
 datasets = pd.load('dgw_datasets.pd')
-dm = pd.load('dgw_pairwise_distances.npy')
+dm = np.load('dgw_pairwise_distances.npy')
 
 hc = dgw.cluster.analysis.HierarchicalClustering(datasets, dm)
 
