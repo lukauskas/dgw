473c93a85d1e1ee1eaa22667ef108adf9288f1ac
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-19 03:33:45 +0000

    better handling of columns

--- dgw/data/parsers/bed.py
+++ dgw/data/parsers/bed.py
@@ -2,6 +2,8 @@ import pandas as pd
 from dgw.data.containers import Regions
 
 __author__ = 'saulius'
+BED_COLUMNS = ['chromosome', 'start', 'end', 'name', 'score',
+               'strand', 'thick_start', 'thick_end', 'item_rgb', 'block_count', 'block_sizes', 'block_starts']
 
 def read_bed(bed_file):
     """
@@ -12,8 +14,9 @@ def read_bed(bed_file):
     """
     regions = pd.read_csv(bed_file, sep="\t", header=None)
 
-    regions.columns = ['chromosome', 'start', 'end', 'name', 'score']
-    regions = regions.set_index('name')
+    regions.columns = BED_COLUMNS[:len(regions.columns)]
+    if 'name' in regions.columns:
+        regions = regions.set_index('name')
 
     return Regions(regions)
 
@@ -24,6 +27,16 @@ def write_bed(regions, file_object):
     for ix, data in regions.iterrows():
 
         row = list(data[['chromosome', 'start', 'end']])
-        row.append(ix)
+        row.append(ix) # Index == name is the fourth positional argument
+
+        # Score is the fifth
+        if 'score' in data.columns:
+            row.append(data['score'])
+
+        if 'strand' in data:
+            if not 'score' in data.columns:
+                row.append(0)  # We do not known the score, so make up a value
+
+            row.append(data['strand'])
 
         writer.writerow(row)
