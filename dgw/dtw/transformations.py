91ef41520f9887a42b71c00f92d47e45043f935e
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-19 19:51:27 +0000

    dgw/dtw/transformations.py now pass all tests

--- dgw/dtw/transformations.py
+++ dgw/dtw/transformations.py
@@ -31,6 +31,8 @@ def uniform_scaling_to_length(sequence, desired_length):
 
 def uniform_shrinking_to_length(sequence, desired_length):
     sequence = _strip_nans(sequence)
+    sequence = np.asarray(sequence, dtype=float)
+    
     current_length = len(sequence)
 
     if current_length == 0:
@@ -46,13 +48,49 @@ def uniform_shrinking_to_length(sequence, desired_length):
     # This is essentially how many points in the current sequence will be mapped to a single point in the newone
     shrink_factor = float(current_length) / desired_length
 
-    new_sequence = np.empty(desired_length)
-    for i in range(desired_length):
-        start = int(floor(i * shrink_factor))
-        end = int(floor((i+1) * shrink_factor))
+    try:
+        ndim = sequence.shape[1]
+    except IndexError:
+        ndim = 0
+    if ndim == 0:
+        new_sequence = np.empty(desired_length)
+    else:
+        new_sequence = np.empty((desired_length, ndim))
 
-        subset = sequence[start:end]
-        new_sequence[i] = np.mean(subset)
+    for i in range(desired_length):
+        start = i * shrink_factor
+        end = (i + 1) * shrink_factor
+
+        s = 0
+        d = 0
+
+        left_bound = int(floor(start))
+
+        if start == left_bound:
+            left_bound_input = 1
+        else:
+            left_bound_input = ceil(start) - start
+
+        if left_bound_input > 0:
+            s += sequence[left_bound] * left_bound_input
+            d += left_bound_input
+
+        right_bound = int(floor(end))
+        right_bound_input = end - floor(end)
+
+        if right_bound_input > 0:
+            print right_bound, right_bound_input, sequence[right_bound], sequence[right_bound] * right_bound_input
+            s += sequence[right_bound] * right_bound_input
+            d += right_bound_input
+
+        for j in xrange(left_bound + 1, right_bound):
+            s += sequence[j]
+            d += 1.0
+
+        assert(abs(d - shrink_factor) < 0.000001)
+
+        new_sequence[i] = s / d
+
     return new_sequence
 
 
