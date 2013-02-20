6e1eb957b9241d90aaca06107e5de6d2026daa19
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-20 11:43:41 +0000

    dgw-workspace now respects prefixes

--- dgw-workspace.py
+++ dgw-workspace.py
@@ -4,8 +4,14 @@ import matplotlib.pyplot as plt
 
 import dgw.cluster.analysis
 
-datasets = pd.load('dgw_datasets.pd')
-dm = np.load('dgw_pairwise_distances.npy')
+import sys
+prefix = 'dgw_' if len(sys.argv) < 2 else sys.argv[1]
+
+# TODO: this should load configuration object up instead
+
+datasets = pd.load('{0}datasets.pd'.format(prefix))
+dm = np.load('{0}pairwise_distances.npy'.format(prefix))
+regions = pd.load('{0}regions.pd'.format(prefix))
 
 hc = dgw.cluster.analysis.HierarchicalClustering(datasets, dm)
 
