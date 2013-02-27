6fe7284fcac742bdd513291e8b109ec0c2caf23b
Saulius Lukauskas <luksaulius@gmail.com>
2013-02-27 01:07:10 +0000

    hcpreviewer now has a cut line

--- dgw/cluster/visualisation.py
+++ dgw/cluster/visualisation.py
@@ -100,8 +100,8 @@ class ClusterPreviewer(object):
 
 
     def show(self):
-        # A5 Paper size
-        plt.figure(num=None, figsize=(8.3, 5.8), facecolor='w', edgecolor='k')
+        # A4 Paper size
+        plt.figure(num=None, figsize=(11.7, 8.3), dpi=100, facecolor='w', edgecolor='k')
         self.title = plt.suptitle("") # Create text object for title
         self.draw_buttons()
         self.draw()
@@ -110,6 +110,10 @@ class ClusterPreviewer(object):
 class HierarchicalClusteringViewer(object):
     _hierarchical_clustering_object = None
     _gs_main = None
+    _figure = None
+    _ax_dendrogram = None
+    _line = None
+    _cut_xdata = 0
 
     def __init__(self, hierarchical_clustering_object):
         self._hierarchical_clustering_object = hierarchical_clustering_object
@@ -123,6 +127,10 @@ class HierarchicalClusteringViewer(object):
         return self.gs_main[1]
 
     @property
+    def ax_dendrogram(self):
+        return self._ax_dendrogram
+
+    @property
     def gs_main(self):
         return self._gs_main
 
@@ -132,22 +140,50 @@ class HierarchicalClusteringViewer(object):
 
     def set_up(self):
         self._gs_main = gridspec.GridSpec(1, 2, wspace=0)
+        self._figure = plt.gcf()
+        self._ax_dendrogram = plt.subplot(self.gs_dendrogram)
+        self._figure.canvas.mpl_connect('button_press_event', self._onclick_listener)
 
-    def draw(self):
-        ax_dendrogram = plt.subplot(self.gs_dendrogram)
+    def draw_dendrogram(self):
+        ax_dendrogram = self.ax_dendrogram
+        plt.subplot(ax_dendrogram)
         hc = self.hierarchical_clustering_object
 
         dendrogram_dict = hc.dendrogram(orientation='right', get_leaves=True,
-                                        color_threshold=0, distance_sort=True)
+                                        distance_sort=True, color_threshold=self._cut_xdata)
         leaves = dendrogram_dict['leaves']
 
+        return leaves
+
+    def draw(self):
+        leaves = self.draw_dendrogram()
+        hc = self.hierarchical_clustering_object
         index = hc.data.items[leaves]
 
         DENDROGRAM_SCALE = 10  # scipy.cluster.hierarachy.dendrogram scales all y axis values by tenfold for some reason
-        hc.data.plot_heatmap(subplot_spec=self.gs_heatmap, no_y_axis=True, sort_by=index, share_y_axis=ax_dendrogram,
+        hc.data.plot_heatmap(subplot_spec=self.gs_heatmap, no_y_axis=True, sort_by=index, share_y_axis=self.ax_dendrogram,
                              scale_y_axis=DENDROGRAM_SCALE)
 
     def show(self):
+        # A5 Paper size
+        plt.figure(num=None, figsize=(8.3, 5.8), dpi=100, facecolor='w', edgecolor='k')
         self.set_up()
         self.draw()
         plt.show()
+
+    def cut_line(self, xdata):
+        if self._line is not None:
+            self._line.set_xdata(xdata)
+        else:
+            self._line = self._ax_dendrogram.axvline(linestyle='--', x=xdata, color='k')
+
+        self._cut_xdata = xdata
+        self.draw_dendrogram()
+
+
+    def _onclick_listener(self, event):
+        # Allow only in axis, only the left button and only regions with value
+        if event.inaxes != self.ax_dendrogram or event.button != 1 or event.xdata is None:
+            return
+        self.cut_line(event.xdata)
+
