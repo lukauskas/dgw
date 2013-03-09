86940fcafe4ad46b8a2cd9b6bc2002fa19e1339c
Saulius Lukauskas <luksaulius@gmail.com>
2013-03-09 00:37:20 +0000

    extract-prototypes nwo gets all images and draws a shiny graph

--- utilities/plotting/extract-prototypes.py
+++ utilities/plotting/extract-prototypes.py
@@ -1,6 +1,9 @@
 import argparse
+import os
 from dgw.cli import StoreFilenameAction
 from dgw.cli.configuration import load_configuration_from_file
+
+import matplotlib.pyplot as plt
 
 __author__ = 'saulius'
 
@@ -16,8 +19,52 @@ def argument_parser():
     parser.add_argument('output_directory', help='Output directory')
     return parser
 
+def graph_from_node_list_to_file(node_list, file_object):
+    file_object.write('graph dgw_clustering {\n')
+    file_object.write('node [style="filled"];')
+
+    for node in node_list:
+        file_object.write('"{0}" [label="{0} - {1}", image="{0}.png"];\n'.format(node.id, node.dist))
+        if not node.is_leaf():
+            file_object.write('"{0}" -- "{1}";\n'.format(node.id, node.get_left().id))
+            file_object.write('"{0}" -- "{1}";\n'.format(node.id, node.get_right().id))
+    file_object.write('}\n')
+
+def prototype_images_from_node_list(node_list, output_directory):
+    print '> Saving images to {0!r}'.format(output_directory)
+    plt.figure(figsize=(3, 2))
+    for node in node_list:
+
+        filename = '{0}.png'.format(node.id)
+        full_filename = os.path.join(output_directory, filename)
+        print '> Saving {0}'.format(full_filename)
+
+        plt.plot(node.prototype)
+        plt.savefig(full_filename)
+        plt.clf()
+
 def main():
     parser = argument_parser()
     args = parser.parse_args()
     configuration = load_configuration_from_file(args.configuration_file)
+
+    hc = configuration.create_hierarchical_clustering_object()
+    tree_nodes = hc.tree_nodes_list
+
+    output_directory = args.output_directory
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    prototype_images_from_node_list(tree_nodes, output_directory)
+
+    print '> Creating a dot graph'
+
+    f = open(os.path.join(output_directory, 'graph.dot'), 'w')
+    graph_from_node_list_to_file(tree_nodes, f)
+    f.close()
+
+    print '> Done'
+
+if __name__ == '__main__':
+    main()
 
