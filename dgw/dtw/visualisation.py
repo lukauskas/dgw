036c93edb92466c64199e9f88c12b814bf2fba37
Saulius Lukauskas <luksaulius@gmail.com>
2013-03-13 00:36:08 +0000

    routines for better DTW visualisation

--- dgw/dtw/visualisation.py
+++ dgw/dtw/visualisation.py
@@ -1,6 +1,8 @@
+from matplotlib.patches import ConnectionPatch
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.ticker import NullFormatter
+import numpy as np
 
 from distance import dtw_std
 
@@ -111,3 +113,122 @@ def visualise_dtw(sequence_x, sequence_y, dtw_function=dtw_std, *args, **kwargs)
     """
     dist, cost, path = dtw_function(sequence_x, sequence_y, dist_only=False)
     plot_dtw_sequences_dist_cost_and_path(sequence_x, sequence_y, dist, cost, path, *args, **kwargs)
+
+def visualise_dtw_mappings(sequence_x, sequence_y, dtw_function=dtw_std, columns=None, title=None, sequence_a_label=None,
+                           sequence_b_label=None):
+
+    def major_tick_step(ax, axis):
+        if axis == 'x':
+            ticks = ax.get_xticks()
+        else:
+            ticks = ax.get_yticks()
+
+        try:
+            step = ticks[1] - ticks[0]
+        except IndexError:
+            step = 0.2
+
+        return step
+
+    def expand_axes(ax):
+        x_increment = major_tick_step(ax, 'x') / 8.0
+
+        min_x, max_x = ax.get_xlim()
+        ax.set_xlim(min_x - x_increment, max_x + x_increment)
+
+        y_increment = major_tick_step(ax, 'y') / 8.0
+        min_y, max_y = ax.get_ylim()
+        ax.set_ylim(min_y - y_increment, max_y + y_increment)
+
+    def add_reversed_annotation(ax):
+        min_x, max_x = ax.get_xlim()
+        min_y, max_y = ax.get_ylim()
+
+        offset_x = major_tick_step(ax, 'x')
+        offset_y = major_tick_step(ax, 'y')
+        ax.text(min_x + offset_x / 8, max_y - offset_y / 2 - offset_y / 8, '(reversed)')
+
+    dist, cost, path = dtw_function(sequence_x, sequence_y, dist_only=False)
+
+    sequence_x = np.asarray(sequence_x)
+    sequence_y = np.asarray(sequence_y)
+
+    try:
+        ndim = sequence_x.shape[1]
+    except IndexError:
+        ndim = 1
+
+    if path[0][0] != 0:
+        reversed = True
+        sequence_x = sequence_x[::-1]
+        path_x = np.max(path[0]) - path[0]
+        path = (path_x, path[1])
+    else:
+        reversed = False
+
+    sequence_y_T = np.atleast_2d(sequence_y.T)
+    sequence_x_T = np.atleast_2d(sequence_x.T)
+
+    if not columns and ndim > 1:
+        columns = ['Dimension #{0}'.format(i) for i in range(1, ndim+1)]
+    elif ndim > 1:
+        if len(columns) != ndim:
+            raise ValueError('Number of column titles does not match the number of dimensions')
+
+    main_y_axis = None
+    xaxes_regular = [None] * ndim
+    xaxes_warped = [None] * ndim
+    figure = plt.figure()
+    figure.subplots_adjust(wspace=0.01, hspace=0.1)
+    for i in range(ndim):
+        x = sequence_x_T[i]
+        y = sequence_y_T[i]
+
+        ax2 = plt.subplot(2, ndim, ndim + i + 1, sharey=main_y_axis, sharex=xaxes_warped[i])
+        ax2.plot(y, color='g')
+
+        if i > 0:
+            ax2.yaxis.set_visible(False)
+
+        expand_axes(ax2)
+
+        if not main_y_axis:
+            main_y_axis = ax2
+
+        if not xaxes_warped[i]:
+            xaxes_warped[i] = ax2
+
+        ax1 = plt.subplot(2, ndim, i + 1, sharey=main_y_axis, sharex=xaxes_regular[i])
+        ax1.plot(x, color='b')
+        expand_axes(ax1)
+
+        if ndim > 1:
+            ax1.set_title(columns[i])
+
+        if i > 0:
+            ax1.yaxis.set_visible(False)
+
+
+        if not xaxes_regular[i]:
+            xaxes_regular[i] = ax1
+        if reversed:
+            add_reversed_annotation(ax1)
+
+        for p_i, p_j in zip(path[0], path[1]):
+            xy_a = (p_i, x[p_i])
+            xy_b = (p_j, y[p_j])
+            con = ConnectionPatch(xyA=xy_a, xyB=xy_b, coordsA="data", coordsB="data",
+                                 axesA=ax1, axesB=ax2, arrowstyle="-", shrinkB=2, shrinkA=2, alpha=0.2)
+
+            ax1.add_artist(con)
+
+    if title is not None:
+        plt.suptitle(title)
+
+    lines = xaxes_regular[0].get_lines()
+    lines.extend(xaxes_warped[0].get_lines())
+
+    if sequence_a_label and sequence_b_label:
+        plt.figlegend(lines, (sequence_a_label, sequence_b_label), 'lower center')
+
+    return figure
